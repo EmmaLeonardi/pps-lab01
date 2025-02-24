c6e5f191882cb9e6a74e5c61b9002684d1265ce5
Emma Leonardi <eleonardi.mobile@gmail.com>
2025-02-24 18:28:56 +0100

    Added test to check if stack is empty

---
+++ tdd-exercise/src/main/java/tdd/implementations/CustomStack.java
@@ -0,0 +1,40 @@
+package tdd.implementations;
+
+import tdd.interfaces.MinMaxStack;
+
+public class CustomStack implements MinMaxStack {
+    @Override
+    public void push(int value) {
+
+    }
+
+    @Override
+    public int pop() {
+        return 0;
+    }
+
+    @Override
+    public int peek() {
+        return 0;
+    }
+
+    @Override
+    public int getMin() {
+        return 0;
+    }
+
+    @Override
+    public int getMax() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
--- tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
@@ -1,12 +1,16 @@
 package tdd;
 
 import org.junit.jupiter.api.Test;
+import tdd.implementations.CustomStack;
+import tdd.interfaces.MinMaxStack;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MinMaxStackImplTest {
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testStackStartsEmpty(){
+        MinMaxStack stack=new CustomStack();
+        assertTrue(stack.isEmpty());
     }
 }
